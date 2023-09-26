b7c7e8860e0779e0da4a47bf7e33c8adf4cb4e71
AjAy Opash <agh.opash@gmail.com>
2023-09-26 16:02:02 +0530

    create main, auth layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./main-layout/main-layout.module').then(m => m.MainLayoutModule),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./auth-layout/auth-layout.module').then(m => m.AuthLayoutModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
