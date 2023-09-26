dc012b34de492897d48a1f6cb77b3aa0aae63d25
AjAy Opash <agh.opash@gmail.com>
2023-09-26 13:38:32 +0530

    fix navbar scroll issue

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -6,51 +6,51 @@ Version: 1.0
 */
 (function($) {
   "use strict"; // Start of use strict
-	
+
 	// Top navigation
-  var lastScrollTop = 0;
-  var $navbar = $('.navbar');
-  var navbarHeight = $navbar.outerHeight();
-  var movement = 0;
-  var lastDirection = 0;
+  // var lastScrollTop = 0;
+  // var $navbar = $('.navbar');
+  // var navbarHeight = $navbar.outerHeight();
+  // var movement = 0;
+  // var lastDirection = 0;
 
-  $(window).scroll(function(event){
-    var st = $(this).scrollTop();
-    movement += st - lastScrollTop;
+  // $(window).scroll(function(event){
+  //   var st = $(this).scrollTop();
+  //   movement += st - lastScrollTop;
 
-    if (st > lastScrollTop) { // scroll down
-      if (lastDirection != 1) {
-        movement = 0;
-      }
-      var margin = Math.abs(movement);
-      if (margin > navbarHeight) {
-        margin = navbarHeight;
-      }
-      margin = -margin;
-      $navbar.css('top', margin+"px")
+  //   if (st > lastScrollTop) { // scroll down
+  //     if (lastDirection != 1) {
+  //       movement = 0;
+  //     }
+  //     var margin = Math.abs(movement);
+  //     if (margin > navbarHeight) {
+  //       margin = navbarHeight;
+  //     }
+  //     margin = -margin;
+  //     $navbar.css('top', margin+"px")
 
-      lastDirection = 1;
-    } else { // scroll up
-      if (lastDirection != -1) {
-        movement = 0;
-      }
-      var margin = Math.abs(movement);
-      if (margin > navbarHeight) {
-        margin = navbarHeight;
-      }
-      margin = margin-navbarHeight;
-      $navbar.css('top', margin+"px")
+  //     lastDirection = 1;
+  //   } else { // scroll up
+  //     if (lastDirection != -1) {
+  //       movement = 0;
+  //     }
+  //     var margin = Math.abs(movement);
+  //     if (margin > navbarHeight) {
+  //       margin = navbarHeight;
+  //     }
+  //     margin = margin-navbarHeight;
+  //     $navbar.css('top', margin+"px")
 
-      lastDirection = -1;
-    }
+  //     lastDirection = -1;
+  //   }
 
-    lastScrollTop = st;
-    // console.log(margin);
-  });
+  //   lastScrollTop = st;
+  //   // console.log(margin);
+  // });
 
-	
+
   // Toggle the side navigation
-   $(document).on('click', '#sidebarToggle', function(e) {  
+   $(document).on('click', '#sidebarToggle', function(e) {
     e.preventDefault();
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
@@ -65,7 +65,7 @@ Version: 1.0
       e.preventDefault();
     }
   });
-  
+
   // Category Owl Carousel
   const objowlcarousel = $('.owl-carousel-category');
   if (objowlcarousel.length > 0) {
@@ -109,7 +109,7 @@ Version: 1.0
       autoplayHoverPause: true,
     });
   }
-	
+
   // Tooltip
   $('[data-toggle="tooltip"]').tooltip()
 
