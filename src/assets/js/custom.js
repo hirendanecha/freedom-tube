da5bd1d267aaa25842e619cfafb7c54b2fa53889
AjAy Opash <agh.opash@gmail.com>
2023-09-26 12:33:45 +0530

    used mobile menu

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -6,7 +6,49 @@ Version: 1.0
 */
 (function($) {
   "use strict"; // Start of use strict
+	
+	// Top navigation
+  var lastScrollTop = 0;
+  var $navbar = $('.navbar');
+  var navbarHeight = $navbar.outerHeight();
+  var movement = 0;
+  var lastDirection = 0;
 
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+    movement += st - lastScrollTop;
+
+    if (st > lastScrollTop) { // scroll down
+      if (lastDirection != 1) {
+        movement = 0;
+      }
+      var margin = Math.abs(movement);
+      if (margin > navbarHeight) {
+        margin = navbarHeight;
+      }
+      margin = -margin;
+      $navbar.css('top', margin+"px")
+
+      lastDirection = 1;
+    } else { // scroll up
+      if (lastDirection != -1) {
+        movement = 0;
+      }
+      var margin = Math.abs(movement);
+      if (margin > navbarHeight) {
+        margin = navbarHeight;
+      }
+      margin = margin-navbarHeight;
+      $navbar.css('top', margin+"px")
+
+      lastDirection = -1;
+    }
+
+    lastScrollTop = st;
+    // console.log(margin);
+  });
+
+	
   // Toggle the side navigation
    $(document).on('click', '#sidebarToggle', function(e) {  
     e.preventDefault();
@@ -67,7 +109,6 @@ Version: 1.0
       autoplayHoverPause: true,
     });
   }
-
 	
   // Tooltip
   $('[data-toggle="tooltip"]').tooltip()
