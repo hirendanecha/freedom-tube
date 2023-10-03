f4c50070aa5c19ecb9bf0ea027f1f5b6c5f7dfd9
Swapnil Opash <sra.opash@gmail.com>
2023-10-03 14:20:53 +0530

    diable auth guard

--- src/app/main-layout/main-layout-routing.module.ts
+++ src/app/main-layout/main-layout-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {
     path: '',
     component: MainLayoutComponent,
-    canActivate: mapToCanActivate([UserAuthGuard]),
+    // canActivate: mapToCanActivate([UserAuthGuard]),
     children: [
       {
         path: '',
