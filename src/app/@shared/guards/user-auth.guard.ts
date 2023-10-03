8863cfb038e824c104e1f778afb841e7ba5662f0
Swapnil Opash <sra.opash@gmail.com>
2023-10-03 15:20:09 +0530

    get token api integrated

--- src/app/@shared/guards/user-auth.guard.ts
+++ src/app/@shared/guards/user-auth.guard.ts
@@ -3,6 +3,7 @@ import { CanActivate, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { CookieService } from 'ngx-cookie-service';
 import { environment } from 'src/environments/environment';
+import { CommonService } from '../services/common.service';
 
 
 @Injectable()
@@ -10,10 +11,18 @@ export class UserAuthGuard implements CanActivate {
     environment = environment
     constructor(
         private authService: AuthService,
+        private commonService: CommonService,
         private router: Router,
     ) { }
 
     canActivate() {
+        const url = environment.apiUrl + 'login/me'
+        let tokenData = {}
+        this.commonService.get(url).subscribe({
+            next: (res) => {
+                return tokenData = res.token
+            }
+        })
         const auth = this.authService.userData();
         const token = this.authService.getToken();
         const isLogin = (token && auth?.Id) || false;
