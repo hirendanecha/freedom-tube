8d6c50ccfb3f68358418e62515b692a142eff7d5
Swapnil Opash <sra.opash@gmail.com>
2023-10-03 16:43:28 +0530

    change redirection

--- src/app/@shared/guards/user-auth.guard.ts
+++ src/app/@shared/guards/user-auth.guard.ts
@@ -7,42 +7,43 @@ import { CommonService } from '../services/common.service';
 
 @Injectable()
 export class UserAuthGuard implements CanActivate {
-  environment = environment;
-  tokenData: any;
-  constructor(
-    private authService: AuthService,
-    private commonService: CommonService,
-    private router: Router
-  ) {
-    const url = environment.apiUrl + 'login/me';
-    this.commonService
-      .get(url, {
-        withCredentials: true,
-      })
-      .subscribe({
-        next: (res) => {
-          console.log('token ==>', res);
-          this.tokenData = res.token;
-        },
-        error: (err) => {
-          console.log(err);
-        },
-      });
-  }
-
-  canActivate() {
-    if (this.tokenData) {
-      const auth = this.tokenData?.user;
-      const token = this.tokenData?.accessToken;
-      const isLogin = (token && auth?.Id) || false;
-      if (isLogin) {
-        return true;
-      } else {
-        location.href = environment?.loginUrl;
-        return false;
-      }
-    } else {
-      return false;
+    environment = environment;
+    tokenData: any;
+    constructor(
+        private authService: AuthService,
+        private commonService: CommonService,
+        private router: Router
+    ) {
+        const url = environment.apiUrl + 'login/me';
+        this.commonService
+            .get(url, {
+                withCredentials: true,
+            })
+            .subscribe({
+                next: (res) => {
+                    console.log('token ==>', res);
+                    this.tokenData = res.token;
+                },
+                error: (err) => {
+                    console.log(err);
+                },
+            });
     }
-  }
+
+    canActivate() {
+        if (this.tokenData) {
+            const auth = this.tokenData?.user;
+            const token = this.tokenData?.accessToken;
+            const isLogin = (token && auth?.Id) || false;
+            if (isLogin) {
+                this.router.navigate(['/home']);
+                return true;
+            } else {
+                location.href = environment?.loginUrl;
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }
