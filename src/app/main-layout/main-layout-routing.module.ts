b32e014f177167c5c32a50462ca57c460b50449f
Swapnil Opash <sra.opash@gmail.com>
2023-10-04 10:02:52 +0530

    auth guard added on home page

--- src/app/main-layout/main-layout-routing.module.ts
+++ src/app/main-layout/main-layout-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
       {
         path: 'home',
         loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
+        canActivate: mapToCanActivate([UserAuthGuard]),
       },
       {
         path: 'channels',
