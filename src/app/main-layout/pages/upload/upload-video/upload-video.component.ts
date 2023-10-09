5a9c26de6498e372368536cfbb0e674262aa2b03
Swapnil Opash <sra.opash@gmail.com>
2023-10-09 11:51:22 +0530

    create post api integration

--- src/app/main-layout/pages/upload/upload-video/upload-video.component.ts
+++ src/app/main-layout/pages/upload/upload-video/upload-video.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { AuthService } from 'src/app/@shared/services/auth.service';
 import { CommonService } from 'src/app/@shared/services/common.service';
+import { ToastService } from 'src/app/@shared/services/toast.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -9,33 +12,41 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./upload-video.component.scss']
 })
 export class UploadVideoComponent {
-  apiUrl = environment.apiUrl + 'posts/video-post'
+  apiUrl = environment.apiUrl + 'posts/create-post'
   videoSize = 0;
   uploadProgress = 80;
-  uploadVideoData: any = {}
+  uploadVideoData: any;
   videoUrl = ''
   postData: any = {
-    profileid: '',
-    communityId: '',
+    profileid: null,
+    communityId: null,
     postdescription: '',
     tags: [],
     imageUrl: '',
     videoduration: '',
     thumbfilename: '',
-    streamname: ''
+    streamname: '',
+    posttype: 'V'
   };
   constructor(
     private commonService: CommonService,
-    private authService: AuthService
+    private authService: AuthService,
+    private toasterService: ToastService,
+    private spinner: NgxSpinnerService,
+    private router: Router
   ) {
     const userData = JSON.parse(this.authService.getUserData() as any)
     this.postData.profileid = userData.profileId
-    this.uploadVideoData = { ...history.state.data };
-    console.log(this.uploadVideoData);
-    this.postData.streamname = URL.createObjectURL(this.uploadVideoData?.file)
-    this.videoSize = this.uploadVideoData?.file?.size / 1024 / 1024;
-    this.postData.videoduration = Math.round(this.uploadVideoData.duration);
-    this.convertBase64ToImage(this.uploadVideoData.thumbfilename);
+    console.log(history.state.data);
+    if (history.state.data) {
+      this.uploadVideoData = { ...history.state.data };
+      this.postData.streamname = URL.createObjectURL(this.uploadVideoData?.file)
+      this.videoSize = this.uploadVideoData?.file?.size / 1024 / 1024;
+      this.postData.videoduration = Math.round(this.uploadVideoData.duration);
+      this.convertBase64ToImage(this.uploadVideoData.thumbfilename);
+    } else {
+      this.router.navigate(['/upload']);
+    }
   }
 
 
@@ -44,22 +55,27 @@ export class UploadVideoComponent {
 
   onSaveClick() {
     console.log(this.postData)
-    // this.commonService.upload(this.uploadVideoData?.file).subscribe({
-    //   next: (res: any) => {
-    //     this.postData.streamname = res.url;
-    //   },
-    //   error: (error) => {
-    //     console.log(error);
-    //   }
-    // });
-
-    // this.commonService.post(this.apiUrl, this.postData).subscribe({
-    //   next: (res: any) => {
-    //     console.log(res)
-    //   }, error: (error) => {
-    //     console.log(error);
-    //   }
-    // })
+    this.spinner.show();
+    this.commonService.upload(this.uploadVideoData?.file).subscribe({
+      next: (res: any) => {
+        this.spinner.hide();
+        this.postData.streamname = res.url;
+        if (this.postData.streamname) {
+          this.commonService.post(this.apiUrl, this.postData).subscribe({
+            next: (res: any) => {
+              this.router.navigate(['/home']);
+              this.toasterService.success('Post created successfully')
+            }, error: (error) => {
+              console.log(error);
+            }
+          })
+        }
+      },
+      error: (error) => {
+        this.spinner.hide();
+        console.log(error);
+      }
+    });
   }
 
   convertBase64ToImage(image: any): void {
