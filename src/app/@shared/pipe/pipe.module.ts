4fc7c0d12cd3882decaca30c5f292ea1672cecd1
Swapnil Opash <sra.opash@gmail.com>
2023-11-28 14:34:52 +0530

    fixed test issue

--- src/app/@shared/pipe/pipe.module.ts
+++ src/app/@shared/pipe/pipe.module.ts
@@ -4,10 +4,11 @@ import { GetImageUrlPipe } from "./get-image-url.pipe";
 import { CommaSeperatePipe } from './comma-seperate.pipe';
 import { TimeDurationPipe, timeDurationWithSec } from './time-duration.pipe';
 import { DateDayPipe } from './date-day.pipe';
+import { NoSanitizePipe } from "./sanitize.pipe";
 
 @NgModule({
-  declarations: [SafePipe, GetImageUrlPipe, CommaSeperatePipe, TimeDurationPipe, timeDurationWithSec, DateDayPipe],
+  declarations: [SafePipe, GetImageUrlPipe, CommaSeperatePipe, TimeDurationPipe, timeDurationWithSec, DateDayPipe, NoSanitizePipe],
   imports: [],
-  exports: [SafePipe, GetImageUrlPipe, CommaSeperatePipe, TimeDurationPipe, timeDurationWithSec, DateDayPipe],
+  exports: [SafePipe, GetImageUrlPipe, CommaSeperatePipe, TimeDurationPipe, timeDurationWithSec, DateDayPipe, NoSanitizePipe],
 })
-export class PipeModule {}
+export class PipeModule { }
