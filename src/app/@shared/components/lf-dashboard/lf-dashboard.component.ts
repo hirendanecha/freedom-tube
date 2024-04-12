e418de9fec5cbb598c04f4a3a2c32a835f0e7633
vatsal <vso.opash@gmail.com>
2024-04-12 13:17:38 +0530

    Fix initial channel load

--- src/app/@shared/components/lf-dashboard/lf-dashboard.component.ts
+++ src/app/@shared/components/lf-dashboard/lf-dashboard.component.ts
@@ -19,6 +19,7 @@ import { VideoPostModalComponent } from '../../modals/video-post-modal/video-pos
 import { CreateChannelComponent } from '../../modals/create-channel/create-channel-modal.component';
 import { ConferenceLinkComponent } from '../../modals/create-conference-link/conference-link-modal.component';
 import { ShareService } from '../../services/share.service';
+import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-lf-dashboard',
@@ -41,6 +42,7 @@ export class LfDashboardComponent implements OnInit {
   channelData: any = {};
   channelList: any = [];
   mediaApproved: boolean;
+  userId: number;
   constructor(
     private route: ActivatedRoute,
     private commonService: CommonService,
@@ -83,7 +85,9 @@ export class LfDashboardComponent implements OnInit {
     if (!this.channelId) {
       this.channelId = +localStorage.getItem('channelId');
     }
-    this.getChannels();
+    if (this.userId) {
+      this.getChannels();
+    }
     this.shareService.mediaApproved$.subscribe(value => {
       this.mediaApproved = value;
     });    
@@ -124,23 +128,54 @@ export class LfDashboardComponent implements OnInit {
   // return this.useDetails?.MediaApproved === 1;
   }
 
+  // openVideoUploadPopUp(): void {
+  //   !this.channelList.length ? this.getChannels() : null
+  //   const modalRef = this.modalService.open(VideoPostModalComponent, {
+  //     centered: true,
+  //     size: 'lg',
+  //   });
+  //   modalRef.componentInstance.title = `Upload Video`;
+  //   modalRef.componentInstance.confirmButtonLabel = 'Upload Video';
+  //   modalRef.componentInstance.cancelButtonLabel = 'Cancel';
+  //   modalRef.componentInstance.channelList = this.channelList;
+  //   modalRef.result.then((res) => {
+  //     if (res === 'success') {
+  //       window.location.reload();
+  //     }
+  //     // console.log(res);
+  //   });
+  // }
   openVideoUploadPopUp(): void {
-    this.channelList.length === 0 ? this.getChannels() : null
-    const modalRef = this.modalService.open(VideoPostModalComponent, {
-      centered: true,
-      size: 'lg',
-    });
-    modalRef.componentInstance.title = `Upload Video`;
-    modalRef.componentInstance.confirmButtonLabel = 'Upload Video';
-    modalRef.componentInstance.cancelButtonLabel = 'Cancel';
-    modalRef.componentInstance.channelList = this.channelList;
-    modalRef.result.then((res) => {
-      if (res === 'success') {
-        window.location.reload();
-      }
-      // console.log(res);
-    });
+    const openModal = () => {
+      const modalRef = this.modalService.open(VideoPostModalComponent, {
+        centered: true,
+        size: 'lg',
+      });
+      modalRef.componentInstance.title = `Upload Video`;
+      modalRef.componentInstance.confirmButtonLabel = 'Upload Video';
+      modalRef.componentInstance.cancelButtonLabel = 'Cancel';
+      modalRef.componentInstance.channelList = this.channelList;
+      modalRef.result.then((res) => {
+        if (res === 'success') {
+          window.location.reload();
+        }
+      });
+    };
+  
+    if (!this.channelList || !this.channelList.length) {
+      this.getChannels().subscribe(
+        () => {
+          openModal();
+        },
+        (error) => {
+          console.error("Error fetching channels:", error);
+        }
+      );
+    } else {
+      openModal();
+    }
   }
+  
 
   createChannel(): void {
     const modalRef = this.modalService.open(CreateChannelComponent, {
@@ -170,17 +205,27 @@ export class LfDashboardComponent implements OnInit {
     });
   }
 
-  getChannels(): void {
-    const userId = JSON.parse(this.authService.getUserData() as any)?.UserID;
-    const apiUrl = `${environment.apiUrl}channels/get-channels/${userId}`;
-    this.commonService.get(apiUrl).subscribe({
-      next: (res) => {
+  // getChannels(): void {
+  //   this.userId = JSON.parse(this.authService.getUserData() as any)?.UserID;
+  //   const apiUrl = `${environment.apiUrl}channels/get-channels/${this.userId}`;
+  //   this.commonService.get(apiUrl).subscribe({
+  //     next: (res) => {
+  //       this.channelList = res.data;
+  //       console.log(this.channelList);
+  //     },
+  //     error(err) {
+  //       console.log(err);
+  //     },
+  //   });
+  // }
+  getChannels(): Observable<any> {
+    this.userId = JSON.parse(this.authService.getUserData() as any)?.UserID;
+    const apiUrl = `${environment.apiUrl}channels/get-channels/${this.userId}`;
+    return this.commonService.get(apiUrl).pipe(
+      map((res) => {
         this.channelList = res.data;
-        console.log(this.channelList);
-      },
-      error(err) {
-        console.log(err);
-      },
-    });
+        return this.channelList;
+      })
+    );
   }
 }
