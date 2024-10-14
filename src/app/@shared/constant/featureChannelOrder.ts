374d2f946432419d4687f29fe95b16ef74973f30
vatsal <vso.opash@gmail.com>
2024-10-14 18:00:11 +0530

    update channel feature channel list

--- src/app/@shared/constant/featureChannelOrder.ts
+++ src/app/@shared/constant/featureChannelOrder.ts
@@ -7,39 +7,39 @@ export const FEATURED_CHANNEL_ORDER: string[] = [
   'Last America Vagabond', //6
   'What is going on?', //6
   'Dollar Vigilante', //7
+  'Larken Rose', //8
   'GeoengineeringWatch.org', //8
   'Gregory Mannarino', //9
   'Solari Report with Catherine Austin Fitts', //10
   'ITM TRADING', //11
-  'Sarah Westall',  //12
+  'Sarah Westall', //12
   'Redpill University', //13
   'The Stew Peters Show', //14
   'SGT Report', //15
-  'Awaken with JP', // 16
+  'Awaken with JP', // 15
+  'TheHealthRanger', //16
   'World Alternative Media', //17
   'Maria Zee', //18
 ];
-  // 'Greg Hunter',
-  // 'Truthstream Media',
-  // 'RonPaulLibertyReport',
-  // 'TheHealthRanger',
-  // 'G Edward Griffin',
-  // 'David Icke',
-  // 'Wes Penre Productions',
-  // 'Dr. Andrew Kaufman',
-  // 'Dr. Sherry Tenpenny',
-  // 'Dr. Rashid Buttar',
-  // 'TruthWillBeKnown',
-  // 'The Inspired Channel',
-  // 'Dr.Robert Young',
-  // 'World Alternative Media',
-  // 'Larken Rose',
-  // "Corey'sDiggs",
-  // 'Truth Lives Here',
-  // 'tammycuthbertgarcia',
-  // ' Dr.TomCowan',
-  // 'ConnectingTheDots',
-  // 'The Morgan Report',
-  // 'The Celestial Report ',
-  // 'CarrieMadej',
-  // 'AnaMihalcea,MD.PhD',
+// 'Greg Hunter',
+// 'Truthstream Media',
+// 'RonPaulLibertyReport',
+// 'G Edward Griffin',
+// 'David Icke',
+// 'Wes Penre Productions',
+// 'Dr. Andrew Kaufman',
+// 'Dr. Sherry Tenpenny',
+// 'Dr. Rashid Buttar',
+// 'TruthWillBeKnown',
+// 'The Inspired Channel',
+// 'Dr.Robert Young',
+// 'World Alternative Media',
+// "Corey'sDiggs",
+// 'Truth Lives Here',
+// 'tammycuthbertgarcia',
+// ' Dr.TomCowan',
+// 'ConnectingTheDots',
+// 'The Morgan Report',
+// 'The Celestial Report ',
+// 'CarrieMadej',
+// 'AnaMihalcea,MD.PhD',
